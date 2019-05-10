7c7d60c7e0d1364118a7f80010493928dde3e4b2
jose gomez <francisco.gomez2101@gmail.com>
2019-05-09 20:00:51 -0500

    2 opciones de menu y combinado

--- src/app/mensaje/mensaje-routing.module.ts
+++ src/app/mensaje/mensaje-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ComponenteMensajeComponent} from './componente-mensaje/componente-mensaje.component';
 
-const routes: Routes = [{
-  path: 'mensaje',
-  component: ComponenteMensajeComponent
-}];
+import { ComponenteMensajeFormComponent } from './componente-mensaje-form/componente-mensaje-form.component';
+import { ComponenteMensajeListaComponent } from './componente-mensaje-lista/componente-mensaje-lista.component';
+import { ComponenteMensajeComponent } from './componente-mensaje/componente-mensaje.component';
+
+const routes: Routes = [
+  {path: 'ingresar', component: ComponenteMensajeFormComponent},
+  {path: 'listar', component: ComponenteMensajeListaComponent},
+  {path: 'combinado', component: ComponenteMensajeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
